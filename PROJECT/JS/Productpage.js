edea974618b2b949afbb4004f075578beddc4102
larry-otieno <larryotieno@kabarak.ac.ke>
2024-09-05 22:35:09 +0300

    Delete PROJECT directory

--- PROJECT/JS/Productpage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import parseHtml from 'react-html-parser';
-import Product_pageHtml from './Product_page.html';
-
-function Product_page() {
-  return parseHtml(Product_pageHtml);
-}
-
-export default Product_page;
